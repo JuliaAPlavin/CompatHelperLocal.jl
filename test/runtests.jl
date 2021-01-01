49a338aac0a065aa41fa4573802dd2e595626c8a
Alexander Plavin <alexander@plav.in>
2021-01-01 00:00:00 +0000

    test on 1.5 as well

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
 using Test
-import CompatHelperLocal as CHL
+import CompatHelperLocal
 
-@time @test CHL.@check()
+@time @test CompatHelperLocal.@check()
 
-@time CHL.check("./test_package_dir/")
+@time CompatHelperLocal.check("./test_package_dir/")
 
 # run(`$(Base.julia_cmd()) ../docs/make.jl`)
