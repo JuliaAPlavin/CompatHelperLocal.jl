c38914241055c7e21891490eadc697366c367a67
Alexander Plavin <alexander@plav.in>
2021-01-01 00:00:00 +0000

    improve printing

--- src/CompatHelperLocal.jl
+++ src/CompatHelperLocal.jl
@@ -39,7 +39,7 @@ function check(pkg_dir::String)
             Pkg.Types.is_stdlib(uuid) && return (; name, compat_state = :stdlib, ok=true)
             compat = get(project.compat, name, "")
             latest = get_latest_version(name)
-            latest == nothing && return (; name, compat_spec = :not_found, ok=false)
+            latest == nothing && return (; name, compat_state = :not_found, ok=false, compat)
             if isempty(compat)
                 return (; name, compat_state = :missing, ok=false, latest)
             else
@@ -50,18 +50,19 @@ function check(pkg_dir::String)
         end
         all(c.ok for c in dep_compats) && continue
         all_ok = false
-        println("Project $f has issues with [compat]:")
+        @warn "Project has issues with [compat]" project=f
         for c in sort(dep_compats, by=c -> (c.compat_state, c.name))
             if c.compat_state == :not_found
-                println("unknown    $(c.name)")
+                @info "package not in registries" name=c.name
             elseif c.compat_state == :missing
-                println("missing    $(c.name)")
+                @info "[compat] missing" name=c.name
             elseif c.compat_state == :outdated
-                println("outdated   $(c.name)    compat=$(c.compat_spec)    latest=$(c.latest)")
+                @info "[compat] outdated" name=c.name compat=c.compat compat_spec=c.compat_spec latest=c.latest
             else
                 @assert c.ok
             end
         end
+        println()
         println("Suggested content:")
         println("[compat]")
         for c in sort(dep_compats, by=c -> c.name)
@@ -71,10 +72,13 @@ function check(pkg_dir::String)
                 println("""$(c.name) = "$(merge_old_new_compat(c.compat, c.latest))" """)
             elseif c.compat_state == :uptodate
                 println("""$(c.name) = "$(c.compat)" """)
+            elseif c.compat_state == :not_found
+                isempty(c.compat) || println("""$(c.name) = "$(c.compat)"  # package not found in registries""")
             else
-                @assert c.compat_state ∈ (:not_found, :stdlib)
+                @assert c.compat_state == :stdlib
             end
         end
+        println()
     end
     return all_ok
 end
