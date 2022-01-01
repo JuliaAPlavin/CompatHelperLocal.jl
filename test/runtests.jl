52ccf386b41b75a31ca5c9a265a41d98266a3724
Alexander Plavin <alexander@plav.in>
2022-01-01 00:00:00 +0000

    better tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using Test
+import Pkg
 import CompatHelperLocal as CHL
 
 @time CHL.@check()
@@ -12,7 +13,18 @@ import CompatHelperLocal as CHL
     @test CHL.CompatStates.generate_new_compat(v"0.1.3"; is_julia=true) == "0.1"
     @test CHL.CompatStates.generate_new_compat(v"1.2.3+5"; is_julia=true) == "1.2"
 
-    dep_compats = CHL.gather_compats("./test_package_dir/Project.toml")
+    projfile = "./test_package_dir/Project.toml"
+    dep_compats = CHL.gather_compats(projfile)
+    
+    compat_dict = CHL.generate_compat_dict(dep_compats)
+    # check that our values can replace existing compat - e.g., types match
+    let
+        proj = Pkg.Types.read_project(projfile)
+        for (k, v) in pairs(compat_dict)
+            Pkg.Operations.set_compat(proj, k, v)
+        end
+    end
+
     compat_block = CHL.generate_compat_block(dep_compats)
     @test occursin(r"""\[compat\]
 CSV = "\d\.\d\.\d+"
