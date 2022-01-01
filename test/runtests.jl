dc0f70aea132f39e7559740ffe6ee9bf78018493
Alexander Plavin <alexander@plav.in>
2022-01-01 00:00:00 +0000

    fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,12 +7,12 @@ import CompatHelperLocal
     dep_compats = CompatHelperLocal.gather_compats("./test_package_dir/Project.toml")
     compat_block = CompatHelperLocal.generate_compat_block(dep_compats)
     @test occursin(r"""\[compat\]
-CSV = "0.8"
-DataFrames = "0.22"
-OrderedCollections = "1.4"
-Scratch = "0.1, 0.2, 1.0"
-xxxPackageXXX = "1.0"
-julia = "1.[67]"
+CSV = "0\.8"
+DataFrames = "1\.\d"
+OrderedCollections = "1\.4"
+Scratch = "0\.1, 0\.2, 1\.0"
+xxxPackageXXX = "1\.0"
+julia = "1\.[67]"
 """, compat_block)
 end
 
